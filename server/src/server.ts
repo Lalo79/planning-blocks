0592d60c113674b7c98cb3277e4b536004e393f5
Eduardo Pacheco <epacheco.leines@gmail.com>
2026-05-09 21:40:00 -0400

    Started server main file

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
